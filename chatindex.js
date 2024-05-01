b81bc83a4da3d85d33fc46bb46bd8f453ad20b9f
cloudbloqavi <50581941+cloudbloqavi@users.noreply.github.com>
2024-05-01 15:38:46 +0530

    support for session memory

--- chatindex.js
+++ chatindex.js
@@ -28,48 +28,41 @@ const {
   SystemMessagePromptTemplate,
 } = require("@langchain/core/prompts");
 
-const chatPromptMemory = new ConversationSummaryBufferMemory({
-  llm: new ChatOpenAI({ temperature: 0.5 }),
-  maxTokenLimit: 1024,
-  returnMessages: true,
-});
-
-const chatPrompt = ChatPromptTemplate.fromMessages([
-  SystemMessagePromptTemplate.fromTemplate(
-    "The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know."
-  ),
-  new MessagesPlaceholder("history"),
-  HumanMessagePromptTemplate.fromTemplate("{input}"),
-]);
-
 const model = new ChatOpenAI({ temperature: 0.5, verbose: true });
-const chain = new ConversationChain({
-  llm: model,
-  memory: chatPromptMemory,
-  prompt: chatPrompt,
-});
-
 
 // Defining an endpoint to handle incoming requests
 app.post("/converse", async (req, res) => {
-  
-
   // Extracting the user's message from the request body
   const message = req.body.message;
+  const systemmsg = req.body.systemmsg;
+  chatPromptMemory = new ConversationSummaryBufferMemory({
+    llm: new ChatOpenAI({ temperature: 0.5 }),
+    maxTokenLimit: 1024,
+    returnMessages: true,
+    memoryKey: "chat_history_" + req.body.id
+  });
+  
+  const chatPrompt = ChatPromptTemplate.fromMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+      systemmsg + " \n\n If the AI does not know the answer to a question, it truthfully says it does not know."
+    ),
+    new MessagesPlaceholder("chat_history_" + req.body.id),
+    HumanMessagePromptTemplate.fromTemplate("{input}"),
+  ]);
+  
+  const chain = new ConversationChain({
+    llm: model,
+    memory: chatPromptMemory,
+    prompt: chatPrompt,
+  });
   
   const res1 = await chain.invoke({ input: message });
-  console.log({ res1 });
+  // console.log({ res1 });
 
-  
   res.send(res1);
 });
 
 // Starting the Express app and listening on port 3000
 app.listen(3000, async () => {
-  await chatPromptMemory.saveContext({ input: "hi" }, { output: "whats up" });
-  await chatPromptMemory.saveContext(
-    { input: "not much you" },
-    { output: "not much" }
-  );
   console.log("Conversational AI assistant listening on port 3000!");
 });
